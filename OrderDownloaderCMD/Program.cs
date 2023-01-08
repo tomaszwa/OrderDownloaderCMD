33abdecdd8f762aa6876fe196f3e82ab77e75dfb
tomaszwa <twasowicz@outlook.com>
2023-01-08 20:30:29 +0100

    SGT connector

--- OrderDownloaderCMD/Program.cs
+++ OrderDownloaderCMD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BaseLinkerApi;
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -12,51 +13,63 @@ using BaseLinkerApi.Common.JsonConverters;
 using RateLimiter;
 using BaseLinkerApi.Requests.Orders;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using ADODB;
 
 namespace BaseLinkerOrderDownloader;
 
 
 class OrderDownloader
 {
-    static void Main(string[] args)
-    {
-        var httpClient = new HttpClient();
-        var connected = new BaseLinkerApiClient(httpClient, "2001423-2006490-YUNDJTP4BBH14NRYKTOFU7VOFRVQQBHUUA7ZWBB6UCG0KVHAXAE13LLOPHDC4IMZ");
-        var request = new GetOrders()
-        {
-            OrderId = 58292809
-        };
-        var result = connected.SendAsync(request).GetAwaiter().GetResult().Orders;
-    }
-}
-
-
-class SubiektConnector
-{
     [STAThread]
     static void Main(string[] args)
     {
-        try
-        {
-            InsERT.GT gt = new InsERT.GT();
-            gt.Produkt = InsERT.ProduktEnum.gtaProduktSubiekt;
-            gt.Serwer = "DESKTOP-PDDM77M\\INSERTGT";
-            gt.Baza = "Testowy";
-            gt.Autentykacja - InsERT.AutentykacjaEnum.gtaAutentykacjaMieszana;
-            gt.Uzytkownik = "sa";
-            gt.Uzytkownik = "";
-            gt.Operator = "Szef";
-            gt.OperatorHaslo = "";
-
-            InsERT.Subiekt subiekt = (InsERT.Subiekt)gt.Uruchom((Int32))InsERT.UruchomDopasujEnum.gtaUruchomDopasuj, (Int32)InsERT.UruchomEnum.gtaUruchomNieArchiwizujPrzyZamykaniu);
-            subiekt.Okno.Widoczne = true;
-        }
-        catch(Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-            Console.ReadKey();
-        }
+        InsERT.GT gt = new InsERT.GT();
+        InsERT.Subiekt sgt;
+        gt.Produkt = InsERT.ProduktEnum.gtaProduktSubiekt;
+        gt.Serwer = "DESKTOP-PDDM77M\\INSERTGT";
+        gt.Baza = "Testowy";
+        gt.Autentykacja = InsERT.AutentykacjaEnum.gtaAutentykacjaWindows;
+        gt.Uzytkownik = "sa";
+        gt.Uzytkownik = "";
+        gt.Operator = "Szef";
+        gt.OperatorHaslo = "";
+        sgt = (InsERT.Subiekt)gt.Uruchom((int)InsERT.UruchomDopasujEnum.gtaUruchomDopasuj, (int)InsERT.UruchomEnum.gtaUruchomNieArchiwizujPrzyZamykaniu);
+        sgt.Okno.Widoczne = true;
     }
+
+        //var httpClient = new HttpClient();
+        //var connected = new BaseLinkerApiClient(httpClient, "2001423-2006490-YUNDJTP4BBH14NRYKTOFU7VOFRVQQBHUUA7ZWBB6UCG0KVHAXAE13LLOPHDC4IMZ");
+        //var request = new GetOrders()
+        //{
+        //    OrderId = 58292809
+        //};
+        //var result = connected.SendAsync(request).GetAwaiter().GetResult().Orders;
+
+
 }
-   
+
+
+
+
+
+//public class Connector
+//{
+//    static int Second()
+//    {
+//        ADODB.Connection conDatabase = new ADODB.Connection();
+//        return 0;
+//    }
+//    public void _Connection.Open(string ConnectionString, string UserID, string Password, int Options)
+
+
+
+
+
+//}
+
+
+
+
+
 
