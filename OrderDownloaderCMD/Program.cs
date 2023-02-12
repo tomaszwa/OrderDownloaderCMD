78b34822ad7eeaacc0643851d8719695d3da9367
tomaszwa <twasowicz@outlook.com>
2023-02-12 14:48:56 +0100

    Downloading and adding ZK to SGT

--- OrderDownloaderCMD/Program.cs
+++ OrderDownloaderCMD/Program.cs
@@ -18,6 +18,10 @@ using ADODB;
 using System.Security.Cryptography.X509Certificates;
 using InsERT;
 using System.Linq;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+using static BaseLinkerApi.Requests.ProductCatalog.GetInventoryProductsPrices;
+using static BaseLinkerApi.Requests.Orders.GetOrderStatusList;
 
 namespace BaseLinkerOrderDownloader;
 
@@ -28,44 +32,75 @@ class OrderDownloader
     public static void Main(string[] args)
     {   
         var subiekt = SgtConnector();
-        var orderJson = ApiConnector();
-        var kontrhanet = KontrahentAdder(subiekt,orderJson);
-        Console.WriteLine(orderJson);
+        var orders = ApiConnector();
+        if (orders != null && orders.Any())
+        {
+            ZKAdder(subiekt, orders);
+            Console.WriteLine("Wysłano do sfery, naura.");
+        }
+        else
+        {
+            Console.WriteLine("Nie ma takiego status elo.");
+        }
     }
-    public static Order ApiConnector()
+
+    public static IEnumerable<Order> ApiConnector()
     {
         var httpClient = new HttpClient();
         var connected = new BaseLinkerApiClient(httpClient, "2001423-2006490-YUNDJTP4BBH14NRYKTOFU7VOFRVQQBHUUA7ZWBB6UCG0KVHAXAE13LLOPHDC4IMZ");
-        var request = new GetOrders()
+        Console.WriteLine("Podaj id statusu z którego pobrać zamówienia:\n");
+        var input = Console.ReadLine();
+        if (int.TryParse(input, out int statusId))
         {
-            OrderId = 58292809
-        };
-        var result = connected.SendAsync(request).GetAwaiter().GetResult().Orders
-            .Select(o => new { Order = o, Product = o.Products.First() })
-            .Select(x => new Order(x.Order.DeliveryFullname, x.Order.DeliveryAddress, x.Product.Name, 
-                x.Product.PriceBrutto, x.Product.TaxRate, x.Product.Quantity));
-        return result.First(); 
+            var request = new GetOrders()
+            {
+                StatusId = statusId
+            };
+            var result = connected.SendAsync(request).GetAwaiter().GetResult().Orders
+                .Select(o => new { Order = o, o.Products })
+                .Select(x => new Order(x.Order.OrderId, x.Order.DeliveryFullname, x.Order.DeliveryAddress, x.Order.DeliveryCity,
+                    x.Products.Select(p => new Product(p.Name, p.PriceBrutto, p.TaxRate, p.Quantity))));
+
+            return result;
+        }
+
+        return null;
     }
 
 
     public class Order
     {
+        public int OrderId { get; set; }
         public string Name { get; set; }
         public string Adress { get; set; }
+        public string DeliveryAdress { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+
+        public Order(int orderId, string name, string adress, string deliveryCity,IEnumerable<Product> products)
+        {
+            OrderId = orderId;
+            Name = name;
+            Adress = adress;
+            DeliveryAdress = deliveryCity;            
+            Products = products;
+
+        }
+
+    }
+
+    public class Product
+    {
         public string ProductName { get; set; }
         public double ProductPrice { get; set; }
         public double ProductTax { get; set; }
         public int ProductQuantity { get; set; }
-        public Order(string name, string adress, string productName, double productPrice, double productTax, int productQuantity)
+        public Product(string productName, double productPrice, double productTax, int productQuantity)
         {
-            Name = name;
-            Adress = adress;
             ProductName = productName;
             ProductPrice = productPrice;
             ProductTax = productTax;
             ProductQuantity = productQuantity;
         }
-
     }
 
 
@@ -76,7 +111,7 @@ class OrderDownloader
         Subiekt sgt;
         gt.Produkt = ProduktEnum.gtaProduktSubiekt;
         gt.Serwer = "DESKTOP-PDDM77M\\INSERTGT";
-        gt.Baza = "Testowy";
+        gt.Baza = "Testowy2";
         gt.Autentykacja = AutentykacjaEnum.gtaAutentykacjaWindows;
         gt.Uzytkownik = "sa";
         gt.Uzytkownik = "";
@@ -89,24 +124,72 @@ class OrderDownloader
     }
 
     public static Kontrahent KontrahentAdder(Subiekt sgt, Order order)
-    {
+    {   
+
         Kontrahent kh;
+        bool check = sgt.KontrahenciManager.Istnieje(order.Name);
+        if (!check)
+        {
+            kh = sgt.KontrahenciManager.DodajKontrahenta();
+            kh.Symbol = order.Name;
+            kh.Nazwa = order.Name;
+            kh.NazwaPelna = order.Name;
+            kh.Ulica = order.Adress;
+            kh.Miejscowosc = order.Name;
 
-        kh = sgt.Kontrahenci.Dodaj();
+            kh.Zapisz();
 
-        kh.Symbol = order.Name;
-        kh.Nazwa = order.Name;
-        kh.NazwaPelna = order.Name;
-        kh.Ulica = order.Adress;
-        kh.Miejscowosc = order.Name;
+            return kh;
+        }       
 
-        kh.Zapisz();
-        kh.Zamknij();
-
-        return kh;
-
-
+        kh = sgt.KontrahenciManager.WczytajKontrahenta(order.Name);
+        return kh; 
     }
+
+    public static void ZKAdder(Subiekt sgt, IEnumerable<Order> orders)
+    {
+        foreach (var order in orders)
+        {
+            SuDokument dokument = sgt.Dokumenty.Dodaj(SubiektDokumentEnum.gtaSubiektDokumentZK);
+            dokument.KontrahentId = KontrahentAdder(sgt, order);
+            dokument.NumerOryginalny = order.OrderId;
+            foreach (var product in order.Products)
+            {
+                SuPozycja tr = dokument.Pozycje.DodajUslugeJednorazowa();
+                tr.UslJednNazwa = product.ProductName;
+                tr.CenaBruttoPrzedRabatem = product.ProductPrice;
+                tr.VatId = product.ProductTax switch
+                {
+                    23 => VatRate.Standard23,
+                    8 => VatRate.Standard8,
+                    4 => VatRate.Standard0,
+                    7 => VatRate.Standard7,
+                    _ => throw new NotSupportedException()
+                };
+                tr.IloscJm = product.ProductQuantity;
+                tr.Jm = "szt.";
+                
+                dokument.Zapisz();
+            }            
+            dokument.Zamknij();
+        }
+    }
+
+
+    public enum VatRate
+    {
+        Standard7 = 2,
+        Standard0 = 4,
+        Standard23 = 100001,
+        Standard8 = 100002,
+        
+    }
+    
+    //public static SuDokument Zamowienie(Subiekt sgt, Order order, KontrahentJednorazowy kh, SuPozycja tr)
+    //{
+    //    SuDokument zk; 
+
+    //}
 
 }
 
